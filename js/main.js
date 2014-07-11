d30bb1be68df1e7ace4ed57193bd2e3c9628ee5c
tiam <tiam_taheri@yahoo.de>
2014-07-12 00:30:09 +0200

    2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+var top = document.scrollTop;
+var width = window.innerWidth;
+var height = window.innerHeight;
+var boundingRect = elem.getBoundingClientRect();
+
+
 $(document).ready(function(){
 	for (var i = 0; i <= 3000; i ++) {
 	console.log(i);
-		$(".wrapper").append("<div>" + i + "</div>");	
+		$(".numbers").append("<li>" + i + "</li>");	
 	}
 });
 
