902ebe530a1d909a25952b846ea5c5b976e737d7
tiam <tiam_taheri@yahoo.de>
2014-07-12 19:33:57 +0200

    11

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-
+//creating array container
 var array = [];
 
+//populating the array
 for (var i = 0; i < 3000; i++) {
 	array = i;
-	console.log(array);
+	//console.log(array);
 }
 
+//displaying 15 elements
+for (var i=0; i<15; i++) {
+	var elements = array[i];
+	console.log(elements);
+}
 
 
