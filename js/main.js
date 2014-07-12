75c6208786b13f2c750b9c1a3514cfc5b0a89b97
tiam <tiam_taheri@yahoo.de>
2014-07-12 14:16:28 +0200

    9

--- js/main.js
+++ js/main.js
@@ -1,23 +1,9 @@
-var top = document.scrollTop;
-var width = window.innerWidth;
-var height = window.innerHeight;
-//var boundingRect = elem.getBoundingClientRect();
+
+var array = [];
+
+for (var i = 0; i < 3000; i++) {
+	array[i] = i;
+	console.log(array);
+}
 
 
-var elements = [];
-
-
-$(document).ready(function(){
-	for (var i = 0; i <= 3000; i ++) {
-	console.log(i);
-		$(".numbers").append("<li>" + i + "</li>");	
-	}
-});
-
-$(function() {
-	//offset
-	var fIOFS = 0; // firstItemOffsetFromStart
-	// Will need to be updated when children changes from dataview
-	var cChildren = $(".container").children();
-
-});
